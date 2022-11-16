15435a81249e17e72ff50293730f9df5fe6083fa
Nolberto Sanchez <nsanchez@mitsoftware.com>
2022-11-16 10:34:26 -0400

    fixed routes

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 // Require express and body-parser
 const express = require("express");
 const bodyParser = require("body-parser");
+const { exec } = require("child_process");
 // Initialize express and define a port
 const app = express();
-const PORT = 3000;
+const PORT = 3344;
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json());
+app.get("/back", async (req, res) => {
+  const { commandText } = req.query;
+  console.log(req.query);
+  await exec(`bash apidev.sh`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.send("some error happened: 😭");
+    }
+
+    return res.status(200).send("ok");
+  });
+});
+app.get("/front", async (req, res) => {
+  const { commandText } = req.query;
+  console.log(req.query);
+  await exec(`bash front.sh`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.send("some error happened: 😭");
+    }
+
+    return res.status(200).send("ok");
+  });
+});
 app.get("/", (req, res) => {
-  console.log(req.body); // Call your action on the request here
-  res.status(200).end(); // Responding is important
+  console.log("corriendo"); // Call your action on the request here
+  //res.status(200).end(); // Responding is important
 });
 app.post("/hook", (req, res) => {
   console.log(req.body); // Call your action on the request here
